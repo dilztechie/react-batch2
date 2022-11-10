15b8f3b7fc466d961aab066dbb914bf804f118b3
Dilz <mail2dilz@gmail.com>
2022-11-10 12:42:25 +0530

    Add files via upload

---
+++ day 02/check.js
@@ -0,0 +1,6 @@
+document.getElementById("display").innerHTML = `
+            <h3>Hello, there!</h3>
+            <p>This is a Demonstration for Displaying anything from the JavaScript</p>
+            <hr />
+            <cite>Copyrights&copy; Reserved to Dilz. Inc.&reg;</cite>
+        `;
