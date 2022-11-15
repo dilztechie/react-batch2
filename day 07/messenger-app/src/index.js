0a0885ee24163ca76775bd2b42e92d9d811bf80d
Dilz <mail2dilz@gmail.com>
2022-11-15 14:01:29 +0530

    Add files via upload

---
+++ day 07/messenger-app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MessageContainer from './components/MessageContainer/MessageContainer';
+import './index.css';
+
+ReactDOM.render(<MessageContainer />, document.getElementById('root'))
